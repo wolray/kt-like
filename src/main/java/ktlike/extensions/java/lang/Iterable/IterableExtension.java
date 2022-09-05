f45454e6c371a378f1cc8f8b39b7139507d84f22
wolray <290061869@qq.com>
2022-09-05 17:42:42 +0800

    [add] Iterable.of

--- src/main/java/ktlike/extensions/java/lang/Iterable/IterableExtension.java
+++ src/main/java/ktlike/extensions/java/lang/Iterable/IterableExtension.java
@@ -9,4 +9,10 @@ public class IterableExtension {
     public static <T> Seq<T> seq(@This Iterable<T> self) {
         return Seq.of(self);
     }
+
+    @Extension
+    @SafeVarargs
+    public static <T> Seq<T> of(T... ts) {
+        return Seq.of(ts);
+    }
 }
