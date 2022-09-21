7344ac6866f559745717203a75ab1f3f09fa4093
ray <wolray@foxmail.com>
2022-09-22 00:26:18 +0800

    [add] Strings remove prefix suffix

--- src/main/java/com/github/wolray/kt/util/Strings.java
+++ src/main/java/com/github/wolray/kt/util/Strings.java
@@ -10,4 +10,25 @@ public class Strings {
     public static String joinBy(String sep, Consumer<StringJoiner> consumer) {
         return Any.also(new StringJoiner(sep), consumer).toString();
     }
+
+    public static String removePrefix(String s, String prefix) {
+        if (s.startsWith(prefix)) {
+            return s.substring(prefix.length());
+        }
+        return s;
+    }
+
+    public static String removeSuffix(String s, String suffix) {
+        if (s.endsWith(suffix)) {
+            return s.substring(0, s.length() - suffix.length());
+        }
+        return s;
+    }
+
+    public static String removeSurrounding(String s, String prefix, String suffix) {
+        if (s.length() >= prefix.length() + suffix.length() && s.startsWith(prefix) && s.endsWith(suffix)) {
+            return s.substring(prefix.length(), s.length() - suffix.length());
+        }
+        return s;
+    }
 }
